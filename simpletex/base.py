2835d648aaaf328f97dbc9d77e0afdc44a79917e
Samuel Li <samuel.wgx@gmail.com>
2016-07-15 22:25:51 -0400

    Style fixes

--- simpletex/base.py
+++ simpletex/base.py
@@ -30,7 +30,7 @@ class OptionFormatter(Formatter):
 class Command(Text):
     """Represents a single LaTeX command."""
 
-    def __init__(self, name: str, arguments=[], *args, **kwargs):
+    def __init__(self, name: str, arguments=(), *args, **kwargs):
         """
         Create a command with the given name, arguments, and options.
 
@@ -49,8 +49,8 @@ class Command(Text):
         """
         super().__init__()
         self.name = name
-        self.arguments = arguments
-        self.options = [args, kwargs]
+        self.arguments = list(arguments)
+        self.options = (args, kwargs)
 
     def __str__(self):
         """Format the command as LaTeX."""
