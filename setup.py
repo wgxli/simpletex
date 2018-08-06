d17eb8fc319b11786239fc54b5df9521e3385b42
Samuel Li <samuel.wgx@gmail.com>
2018-08-06 11:40:48 -0400

    Fixed PyPI setup configuration

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='simpletex',
-      version='0.2.1',
+      version='0.2.3',
       description='Easy Latex Document Generation',
+      long_description=open('README.rst').read(),
       author='Samuel Li',
       author_email='samuel.wgx@gmail.com',
-      url='https://github.com/swe100/simpletex',
-      download_url='https://github.com/swe100/simpletex/archive/v0.1.2.tar.gz',
+      url='https://github.com/wgxli/simpletex',
+      download_url='https://github.com/wgxli/simpletex/archive/v0.2.3.tar.gz',
       packages=find_packages())
